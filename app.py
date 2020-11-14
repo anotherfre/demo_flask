79a948c2461a0d6e152ccd854d1f5478f2f1218c
anotherfre <59173871+anotherfre@users.noreply.github.com>
2020-11-15 02:18:26 +0800

    file

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return '<center><h1>Hello World!ssadasd</h1></center>'
+# -*- coding: utf-8 -*-
+from app import app
 
 
 if __name__ == '__main__':

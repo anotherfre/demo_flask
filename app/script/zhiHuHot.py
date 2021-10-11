03e19dad03ac4c1f7bf6cae8aa298a868a15058f
anotherfre <1225977549@qq.com>
2021-10-11 22:31:53 +0800

    celery 知乎定时任务

--- app/script/zhiHuHot.py
+++ app/script/zhiHuHot.py
@@ -6,23 +6,20 @@ from lxml import etree
 class ZhihuHot:
     def __init__(self):
         self.url = 'https://www.zhihu.com/hot'
-        self.url = 'https://www.zhihu.com/question/318185970/answer/742914625'
-        self.header_data = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                            'Accept-Encoding': 'gzip, deflate, sdch, br',
-                            'Accept-Language': 'zh-CN,zh;q=0.8',
-                            'Connection': 'keep-alive',
-                            'Cache-Control': 'max-age=0', 'Host': 'www.zhihu.com', 'Upgrade-Insecure-Requests': '1',
-                            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-                            }
+        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'}
+        self.proxies = {
+            'http': 'http://122.114.31.177:808'
+        }
 
     def download_item(self):
-        resp = requests.get(self.url, headers=self.header_data)
+        resp = requests.get(self.url, headers=self.headers, allow_redirects=False, proxies=self.proxies)
         if resp.status_code == 200:
-            return resp.content
+            resp.encoding = 'utf-8'
+            return resp.text
 
     def clear_item(self, html):
-        html = etree.parse(html, etree.HTMLParser())
-        result = html.xpath('//section[@class="HotItem"]//text()')
+        html = etree.HTML(html)
+        result = html.xpath("//div[@class='HotItem-content']")
         pass
 
     def save_item(self):

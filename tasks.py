b5b011970a51862e70fd85a7599281bc89b83eda
anotherfre <1225977549@qq.com>
2021-10-15 23:59:54 +0800

    utc时区

--- tasks.py
+++ tasks.py
@@ -11,10 +11,13 @@ backend = 'redis://192.168.76.128:6379/1'
 app = Celery('tasks', broker=brokers, backend=backend)
 
 # app.config_from_object('celery_config')
+# app.conf.timezone = 'Asia/Shanghai'
+# CELERY_TIMEZONE = 'Asia/Shanghai'
+# celery 使用的是UTC时间 比北京时间慢8小时
 app.conf.beat_schedule = {
-    "task_10s": {
-        "task": "tasks.add",
-        "schedule": 30,
+    "celery_task": {
+        "task": "tasks.zhihu_task",
+        "schedule": crontab(hour="4,16", minute=30),
         "args": (10, 10)
     }
 }

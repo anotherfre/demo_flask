777a8839ee4627c5a456ce91a2a423c83add7b27
anotherfre <1225977549@qq.com>
2021-10-17 00:43:15 +0800

    '删除爬虫配置'

--- spider_config.py
+++
@@ -1,8 +0,0 @@
-HOST = '192.168.76.128'
-DATABASE = 'demo_database'
-USER = 'root'
-PASSWORD = '123456789cJ.'
-PORT = 3306
-
-user_agent = """Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"""
-cookie = """"""
